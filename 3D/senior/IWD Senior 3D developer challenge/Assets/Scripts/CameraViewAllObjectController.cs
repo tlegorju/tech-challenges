c3e3035f7471a84f5b914db48d3e21d882957c96
tlegorju <thibault.legorju@gmail.com>
2019-10-25 14:19:59 +0200

    Premier fonctionnel

---
+++ 3D/senior/IWD Senior 3D developer challenge/Assets/Scripts/CameraViewAllObjectController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewAllObjectController : MonoBehaviour
+{
+    private Plane[] planes;
+    public Plane[] Planes { get { return planes; } }
+
+    [SerializeField] private Camera thisCamera;
+
+    private Vector3 previousPos;
+    private Vector3 targetPoint;
+    public Vector3 TargetPoint { get { return targetPoint; } set { targetPoint = value; } }
+
+    [SerializeField] ObjectBoundsManager objectBoundsManager;
+    [SerializeField] float lerpSpeed = 5.0f;
+
+    private void Awake()
+    {
+        thisCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPos = transform.position;
+        UpdatePlanes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(previousPos != transform.position)
+        {
+            previousPos = transform.position;
+            UpdatePlanes();
+        }
+    }
+
+    public void UpdatePlanes()
+    {
+        planes = GeometryUtility.CalculateFrustumPlanes(thisCamera);
+    }
+
+    public void CalculateTargetPoint(Vector3 minBounds, Vector3 maxBounds)
+    {
+        targetPoint.z = (minBounds.z + maxBounds.z) / 2; //minBounds.z-10;
+        targetPoint.x = (minBounds.x + maxBounds.x) / 2;
+        targetPoint.y = (minBounds.y + maxBounds.y) / 2;
+        Debug.Log(targetPoint);
+        StartCoroutine("GoToMiddlePoint");
+    }
+
+    IEnumerator GoToMiddlePoint()
+    {
+        while (Vector3.SqrMagnitude(transform.position - targetPoint) > 0.35f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPoint, Time.deltaTime);
+            yield return null;
+        }
+        StartCoroutine("GoToTargetPoint");
+    }
+
+    IEnumerator GoToTargetPoint()
+    {
+        while(!objectBoundsManager.AllObjectInSight())
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position- transform.TransformDirection(Vector3.forward)*3, Time.deltaTime*lerpSpeed);
+            yield return null;
+        }
+        targetPoint = transform.position - transform.TransformDirection(Vector3.forward) * 3;
+
+        while(Vector3.SqrMagnitude(transform.position-targetPoint)>0.3)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPoint, Time.deltaTime * lerpSpeed);
+            yield return null;
+        }
+    }
+}
---
+++ 3D/senior/IWD Senior 3D developer challenge/Assets/Scripts/ObjectBoundsManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBoundsManager : MonoBehaviour
+{
+    [SerializeField] List<Collider> listObjectCollider;
+    [SerializeField] List<bool> listObjectInSight;
+
+    CameraViewAllObjectController cameraController;
+
+    public Material[] listMaterial;
+
+    Vector3 middlePointObjects = Vector3.zero;
+    Vector3 minBounds, maxBounds;
+
+    private void Awake()
+    {
+        listObjectCollider = new List<Collider>();
+        listObjectInSight = new List<bool>();
+
+        cameraController = Camera.main.GetComponent<CameraViewAllObjectController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] listGameObject = GameObject.FindGameObjectsWithTag("ObjectToDisplay");
+        for(int i=0; i<listGameObject.Length; i++)
+        {
+            Collider tmp = listGameObject[i].GetComponent<Collider>();
+            if (tmp != null)
+            {
+                if(i==0)
+                {
+                    minBounds = maxBounds = tmp.transform.position;
+                }
+                else
+                {
+                    UpdateObjectsBounds(tmp.transform);
+                }
+
+                listObjectCollider.Add(tmp);
+                listObjectInSight.Add(IsObjectInSight(tmp));
+
+                //middlePointObjects += listObjectCollider[i].transform.position;
+            }
+        }
+        middlePointObjects /= listObjectCollider.Count;
+
+        cameraController.CalculateTargetPoint(minBounds, maxBounds);
+    }
+
+    void UpdateObjectsBounds(Transform obj)
+    {
+        if (minBounds.x > obj.position.x)
+            minBounds.x = obj.position.x;
+        if (minBounds.y > obj.position.y)
+            minBounds.y = obj.position.y;
+        if (minBounds.z > obj.position.z)
+            minBounds.z = obj.position.z;
+        if (maxBounds.x < obj.position.x)
+            maxBounds.x = obj.position.x;
+        if (maxBounds.y < obj.position.y)
+            maxBounds.y = obj.position.y;
+        if (maxBounds.z < obj.position.z)
+            maxBounds.z = obj.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///TMP
+        
+        for(int i=0; i<listObjectCollider.Count; i++)
+        {
+
+            listObjectInSight[i] = IsObjectInSight(listObjectCollider[i]);
+            if (listObjectInSight[i])
+            {
+                listObjectCollider[i].GetComponent<MeshRenderer>().material = listMaterial[0];
+            }
+            else
+            {
+                listObjectCollider[i].GetComponent<MeshRenderer>().material = listMaterial[1];
+            }
+        }
+
+
+        //Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, middlePointObjects, 1 * Time.deltaTime);
+
+        ///TMP
+    }
+
+    public bool IsObjectInSight(Collider objCollider)
+    {
+        return GeometryUtility.TestPlanesAABB(cameraController.Planes, objCollider.bounds);
+    }
+
+    public bool AllObjectInSight()
+    {
+        for (int i = 0; i < listObjectInSight.Count; i++)
+            if (!listObjectInSight[i])
+                return false;
+
+        return true;
+    }
+}
